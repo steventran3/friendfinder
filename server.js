b472b2e2ded3f01040bfae7ded9de93eaa4854fb
STEVEN TRAN <StevenTran@STEVENs-MacBook-Pro-2.local>
2018-01-18 15:04:55 -0800

    changes to json package

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ let bodyParser = require('body-parser')
 
 
 let app = express()
-const PORT = 3700
+const PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: true }));
